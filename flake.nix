748b94769703530ff02fa0dbe34df9c73fc157ea
Robert Nolte <robert.nolte@proton.me>
2025-03-15 18:45:46 +0100

    ci(formatter): add formatter via nix

--- flake.nix
+++ flake.nix
@@ -9,5 +9,8 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
-      in { devShells.default = import ./shell { inherit pkgs; }; });
+      in {
+        devShells.default = import ./shell { inherit pkgs; };
+        formatter = pkgs.stylua;
+      });
 }
